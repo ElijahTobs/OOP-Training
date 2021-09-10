951efde0993bfde94ec4eb531f3b939ff45a95c1
Elijah <eligenius@gmail.com>
2021-09-10 08:20:55 +0100

    LClear linter error

--- example_school_library_decorator/person.rb
+++ example_school_library_decorator/person.rb
@@ -4,7 +4,7 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @corrector = Corrector.new
     @age = age
@@ -13,7 +13,7 @@ class Person
   end
 
   def can_use_services?
-    @parent_permission || is_of_age?
+    @parent_permission || of_age?
   end
 
   def validate_name
@@ -22,7 +22,7 @@ class Person
 
   private
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 end
