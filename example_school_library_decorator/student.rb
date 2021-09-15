921d1677be94c63f55212634e4f1a129e8cdba16
ZawadiSumba66 <rosesumba98@gmail.com>
2021-09-15 13:47:10 +0300

    Fix linters errors

--- example_school_library_decorator/student.rb
+++ example_school_library_decorator/student.rb
@@ -3,7 +3,7 @@ require_relative 'classroom'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
